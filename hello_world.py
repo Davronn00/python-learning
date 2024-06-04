6b4d21913ef6cb764b648b4389813f2a4c3fd1d2
davronn00 <tugairoq04@gmail.com>
2024-06-03 22:23:52 -0700

    initial commit

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,5 @@
 #new project
+
 ism = 'Yago'
 yosh = 22
 print(ism)
@@ -7,4 +8,165 @@ print(yosh)
 ism = 'abdulloh'
 print(ism)
 ism= 'tony'
-print(ism)`1`
+print(ism)
+a = 3
+b = 4
+c = (a+b)**2
+print(c)
+p = "U+1F605"
+print(p)
+ism = "ahmad"
+print("uning ismi " +  ism)
+familiya = 'yassaviy'
+print(ism + ' ' + familiya)
+
+
+kocha = "Mevazor"
+mahalla = "Chinor"
+tuman = "Ohangaron"
+viloyat = "Toshkent"
+manzil = f"{viloyat} viloyati, {tuman} tumani, {mahalla} mahallasi, {kocha} ko'chasi"
+print(manzil.title())
+
+print("Iltimos manzilingizni kiriting: ")
+kocha = input("kochangiz")
+mahalla = input("mahallangiz")
+tuman = input("tumaningiz")
+viloyat = input("viloyatingiz")
+print("Demak, sizning manzilingiz: " + viloyat.capitalize() + 
+" viloyati " + tuman.capitalize() +" tumani " + mahalla.capitalize() + 
+" mahallasi " + kocha.title() + " kochasi ")
+a = 20
+b = 5.5
+print(type(a))
+print("Hayrli tong! tony")
+
+hello = "hello tony"
+print(hello)
+
+print("  hello world")
+print("""wijsbwbdibisbf djj dijwbf ijfeofjb 
+      pofiejfiorej oieoifoier""")
+ism = "tony"
+fAMILIYA = "JON"
+print(F"{ism}{fAMILIYA}")
+print(ism + fAMILIYA)
+ism = input("ismingiz nima ? \n >>> ".capitalize())
+soz = "qalesan"
+print("Salom " + ism.capitalize() + "\tqalesan")
+
+a = 20 
+b = 5.5
+temp = 36.6
+aholi_soni =  7_545_676_567
+x, y,z,=45, 5.5,-56
+c=a*b
+print(c)
+
+radius = 20
+PI = 3.14159
+diametr =2*radius 
+
+print("aylana uznligi ",diametr * PI, "ga teng")
+
+ism = "komol"
+yosh = 54
+
+xabar = ism + " " + str(yosh) + " yoshda"
+print(xabar)
+
+son=input("Sonni kiriting ")
+print(int(son)**2)
+ism = input("nimadir: ")
+print (ism.upper())
+
+son7 = input("Istalgan son kiriting: \n>>> ")
+print(son7, " ning kvadrati", int(son7)**2, "ga teng")
+print(son7, " ning kubi esa", int(son7)**3, 'ga teng')
+
+foyd_yoshi = input('Yoshingizni kiriting: \n>>> ')
+tug_yil = 2024 - int(foyd_yoshi)
+
+print("siz",str((tug_yil)).strip(),'- yilda tug\'ilgansiz')
+
+son1 = float(input('birinchi sonni kiriting: \n>>>'))
+son2 = float(input('ikkinchi sonni kiriting: \n>>>'))
+
+amal1 = float(son1)
+amal2 = float(son2)
+print(amal1, '+', amal2, '=', amal1+amal2) 
+print(amal1, '/', amal2, '=', amal1/amal2)
+print(amal1, '*', amal2, '=', amal1*amal2)
+print(amal1, '-', amal2, '=', amal1-amal2)
+print(f"{son1} * {son2} =", son1 * son2)
+print(f"{son1} / {son2} =", son1/son2)
+print(f"{son1} + {son2} =",  son1+son2)
+print(f"{son1} - {son2} =",  son1-son2)
+mevalar = ['olma',input('sevimli mevangizni kiriting: '),'qulupnay']
+narxlar = [2302,30023,320302,3233,9969]
+ismlar = []
+
+mevalar[0]= 'anor'
+
+print(str(mevalar[-2]).title())
+print('sizning sevimli moxitongizni tayyorlash uchun', mevalar, '\n kabi mevalar kerak bo\'ladi')
+
+Sirojiddin = 'yago'
+Fathiddin = 'chingiz galaktika'
+Isfandiyor = 'terminator'
+Samandar= 'sam moy'
+Ohunjon = 'bratishka'
+Zufar = 'roboshop'
+Davron = 'men'
+yangi_bola = input("""ismingizni kiriting: 
+>>> """).capitalize()
+
+xonadagi_bolalalar=[Sirojiddin,Fathiddin,Davron,Zufar,Isfandiyor,Samandar,Ohunjon]
+xonadagi_bolalalar.append(yangi_bola)
+print('yangi kelgan bola bilan birgalikda ',xonadagi_bolalalar)
+
+
+print("Xonadagi umumiy bolalar:", xonadagi_bolalalar)
+
+print('yangi kelgan bolaning laqabi:', yangi_bola, "ko\'cha")
+
+Ohunjon= f"{yangi_bola}{" ko'cha"}"
+xonadagi_yangi_bolalalar = [Sirojiddin, Fathiddin,Davron,Zufar,Isfandiyor,Samandar,Ohunjon]
+print("Xonadagi umumiy bolalar:", xonadagi_yangi_bolalalar)
+
+xonadagi_bolalalar.insert(0,yangi_bola)
+print (xonadagi_bolalalar)
+cars = []
+cars.append('lacetti')
+cars.append('damas')
+cars.append('nexia')
+cars.append('matiz')
+cars.append('spark')
+del cars[0]
+cars.insert(0,'gentra')
+cars.remove("matiz")
+print(cars)
+
+hayvonlar=['chumchuq', 'it', 'mushuk', 'sigir', 'echki', 'chumchuq']
+
+
+hayvonlar.remove('chumchuq')
+print(hayvonlar)
+bozorlik =[" ",'kartoshka','sabzi', 'piyoz','go\'sht','banan']
+mahsulot = bozorlik.pop(int(input('Mahsulot tartib raqamini tanlang \n>>> ')))
+mahsulot = bozorlik.pop(input('Mahsulot tartib raqamini tanlang \n>>> '))
+
+print ("Men bozordan ", mahsulot, "sotib oldim")
+
+ 
+
+
+
+
+           
+
+
+
+
+
+
